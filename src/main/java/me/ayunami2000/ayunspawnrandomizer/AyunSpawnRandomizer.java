04bfee5ed06833ea963fffb288feaa2655ae3df8
ayunami2000 <spwilliamsiam@gmail.com>
2022-04-28 16:21:28 -0400

    more efficient + commented out & slow code

--- src/main/java/me/ayunami2000/ayunspawnrandomizer/AyunSpawnRandomizer.java
+++ src/main/java/me/ayunami2000/ayunspawnrandomizer/AyunSpawnRandomizer.java
@@ -1,5 +1,6 @@
 package me.ayunami2000.ayunspawnrandomizer;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -8,13 +9,15 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
-import java.util.Random;
+import java.util.*;
 
 public final class AyunSpawnRandomizer extends JavaPlugin implements Listener {
     private static final Random random = new Random();
 
-    private static int minX,minZ,maxX,maxZ,y;
+    private static int minX,minZ,maxX,maxZ,y,loaded;
     private static World world;
+    //private static Set<Chunk> safeLoadedChunks = new HashSet<>();
+    //private static Set<Chunk> checkedChunks = new HashSet<>();
 
     @Override
     public void onEnable() {
@@ -24,22 +27,86 @@ public final class AyunSpawnRandomizer extends JavaPlugin implements Listener {
         maxX = getConfig().getInt("max.x");
         maxZ = getConfig().getInt("max.z");
         y = getConfig().getInt("y");
+        loaded = getConfig().getInt("loaded");
         world = getServer().getWorld(getConfig().getString("world"));
         getServer().getPluginManager().registerEvents(this, this);
+        /*
+        int radius = getConfig().getInt("radius");
+        int limit = getConfig().getInt("limit");
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            Chunk[] loadedChunks = world.getLoadedChunks();
+            if (loadedChunks.length < loaded) {
+                safeLoadedChunks.clear();
+                safeLoadedChunks = new HashSet<>(Arrays.asList(loadedChunks));
+            } else {
+                Set<Chunk> newSafeLoadedChunks = new HashSet<>();
+                for (int i = 0; i < Math.min(limit == 0 ? loadedChunks.length : limit, loadedChunks.length); i++) {
+                    Chunk currChunk = loadedChunks[i];
+                    if (nearChunksAreLoaded(currChunk, radius)) {
+                        newSafeLoadedChunks.add(currChunk);
+                    }
+                }
+                safeLoadedChunks.clear();
+                safeLoadedChunks = newSafeLoadedChunks;
+            }
+        }, 0, getConfig().getInt("often"));
+        */
     }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+        //getServer().getScheduler().cancelTasks(this);
+    }
 
     @EventHandler
     public void onPlayerSpawn(PlayerSpawnLocationEvent event){
-        event.setSpawnLocation(randomLocation(event.getSpawnLocation().getWorld()));
+        event.setSpawnLocation(randomLoadedLocation(event.getSpawnLocation().getWorld()));
     }
 
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event){
-        event.setRespawnLocation(randomLocation(world));
+        event.setRespawnLocation(randomLoadedLocation(world));
     }
+
+    private static Location randomLoadedLocation(World world){
+        Chunk[] loadedChunks = world.getLoadedChunks();
+        if (loadedChunks.length < loaded) {
+            return randomLocation(world);
+        } else {
+            /*
+            int index = random.nextInt(safeLoadedChunks.size());
+            Iterator<Chunk> iter = safeLoadedChunks.iterator();
+            for (int i = 0; i < index; i++) iter.next();
+            Chunk chosenChunk = iter.next();
+            */
+            Chunk chosenChunk = loadedChunks[random.nextInt(loadedChunks.length)];
+            return new Location(world, chosenChunk.getX() * 16, y, chosenChunk.getZ() * 16);
+        }
+    }
+
+    /*
+    private static boolean nearChunksAreLoaded(Chunk chunk, int radius){
+        try {
+            for (int ix = -1; ix <= 1; ix++) {
+                for (int iy = -1; iy <= 1; iy++) {
+                    /*
+                    int fix = ix;
+                    int fiy = iy;
+                    Chunk theChunk = checkedChunks.stream().filter(ch -> ch.getX() == fix && ch.getZ() == fiy).findFirst().orElse(null);
+                    if (theChunk == null) {
+                        theChunk = chunk.getWorld().getChunkAt(chunk.getX() + ix, chunk.getZ() + iy);
+                    }
+                    if (!theChunk.isLoaded()) return false;
+                    *//*
+                    if (!chunk.getWorld().getChunkAt(chunk.getX() + ix, chunk.getZ() + iy).isLoaded()) return false;
+                }
+            }
+            return true;
+        } catch (Exception e) { // anticipated edge of world chunks
+            return false;
+        }
+    }
+    */
 
     private static Location randomLocation(World world){
         return new Location(world, random.nextInt(maxX - minX) + minX, y, random.nextInt(maxZ - minZ) + minZ);
